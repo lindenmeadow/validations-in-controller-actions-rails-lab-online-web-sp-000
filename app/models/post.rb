8d996864d3e5502e17e3563d666c977982b21c31
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-04 18:00:42 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, presence: true, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, presence: true, length: { minimum: 100 }
 end
