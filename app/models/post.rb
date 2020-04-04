70e35b9691bd97a72e82e60f1e4eee43376f1d7c
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-04 17:52:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  alidates :title, presence: true
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: { minimum: 100 }
 end
